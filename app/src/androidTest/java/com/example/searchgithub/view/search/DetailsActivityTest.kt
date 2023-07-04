3caf290a68869425427979cd297eb278b96a9971
anton-tchaikovsky <106504887+anton-tchaikovsky@users.noreply.github.com>
2023-07-04 08:09:45 +0300

    Сделал рефакторинг espresso-теста, созданного по методичке.

--- app/src/androidTest/java/com/example/searchgithub/view/search/DetailsActivityTest.kt
+++ app/src/androidTest/java/com/example/searchgithub/view/search/DetailsActivityTest.kt
@@ -1,8 +1,6 @@
 package com.example.searchgithub.view.search
 
 
-import android.view.View
-import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.*
@@ -11,10 +9,8 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.example.searchgithub.R
-import org.hamcrest.Description
-import org.hamcrest.Matcher
+import com.example.searchgithub.view.details.DetailsActivity
 import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,84 +21,27 @@ class DetailsActivityTest {
 
     @Rule
     @JvmField
-    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+    var mActivityScenarioRule = ActivityScenarioRule(DetailsActivity::class.java)
 
     @Test
     fun detailsActivityTest() {
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        Thread.sleep(500)
 
-        val materialButton = onView(
-            allOf(
-                withId(R.id.toDetailsActivityButton), withText("to details"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    3
-                ),
-                isDisplayed()
-            )
-        )
-        materialButton.perform(click())
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        Thread.sleep(700)
-
-        val materialButton2 = onView(
+        val incrementButton = onView(
             allOf(
                 withId(R.id.incrementButton), withText("+"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    2
-                ),
                 isDisplayed()
             )
         )
-        materialButton2.perform(click())
+       incrementButton.perform(click())
 
         val textView = onView(
             allOf(
                 withId(R.id.totalCountTextView), withText("Number of results: 1"),
-                withParent(withParent(withId(android.R.id.content))),
                 isDisplayed()
             )
         )
         textView.check(matches(withText("Number of results: 1")))
 
-        val button = onView(
-            allOf(
-                withId(R.id.incrementButton), withText("+"),
-                withParent(withParent(withId(android.R.id.content))),
-                isDisplayed()
-            )
-        )
-        button.check(matches(isDisplayed()))
     }
 
-    private fun childAtPosition(
-        parentMatcher: Matcher<View>, position: Int
-    ): Matcher<View> {
-
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
-    }
 }
